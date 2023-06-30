2a2d848c2c1a6ed4fb03affa002afdf65d35403a
muratmnz <minazzmurat@gmail.com>
2023-06-30 14:25:21 +0300

    feature :  added Image , font and using VStack , overlay, Circle image , clip shape props.

--- IOSDevCard/ContentView.swift
+++ IOSDevCard/ContentView.swift
@@ -11,10 +11,24 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color(UIColor(red: 0.95, green: 0.77, blue: 0.06, alpha: 1.00)).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("murat")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 3)
+                    )
                 Text("Murat Minaz")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .bold()
-
+                    .font(Font.custom("Lobster-Regular", size: 40))
+                    .bold()
+                .foregroundColor(.white)
+                Text("iOS Developer")
+                    .bold()
+                    .foregroundColor(.white)
+                    .font(.system(size: 20))
+            }
         }
     }
 }
