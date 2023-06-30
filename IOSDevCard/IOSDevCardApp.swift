fe0621bf2ca6df100011e6292cbe7fd68d120723
muratmnz <minazzmurat@gmail.com>
2023-06-30 12:56:07 +0300

    Initial Commit

---
+++ IOSDevCard/IOSDevCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSDevCardApp.swift
+//  IOSDevCard
+//
+//  Created by Murat Minaz on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct IOSDevCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
